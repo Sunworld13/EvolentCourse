579cac3a4fb8e69e9a8a55c15dd9aaa3482bb89a
Яромир <106832565+Sunworld13@users.noreply.github.com>
2025-03-22 11:09:00 +0300

    Add files via upload
    4.9 Задание 12. Инверсия контроля. Внедрение зависимостей

---
+++ Task3/ex12/Multiplier.java
@@ -0,0 +1,8 @@
+package ex12;
+
+public class Multiplier implements Operation {
+    @Override
+    public double getResult(double a, double b) {
+        return a * b;
+    }
+}
---
+++ Task3/ex12/Subtractor.java
@@ -0,0 +1,8 @@
+package ex12;
+
+public class Subtractor implements Operation {
+    @Override
+    public double getResult(double a, double b) {
+        return a - b;
+    }
+}
