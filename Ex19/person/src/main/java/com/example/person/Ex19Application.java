16f020d61b63b82f3361d26a23bb27026bd1271f
Яромир <chesnokov.yaromir@gmail.com>
2025-04-09 13:04:01 +0300

    5.2 Задание 19. Создать веб-службы location и weather.

---
+++ Ex19/person/src/main/java/com/example/person/Ex19Application.java
@@ -0,0 +1,13 @@
+package com.example.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex19Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex19Application.class, args);
+    }
+
+}
---
+++ Ex19/person/src/test/java/com/example/person/Ex19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex19ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
